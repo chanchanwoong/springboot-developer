17df38e24cc57ed71b96f2f196943b5f78441916
chanchanwoong <cksdnddlek99@gmail.com>
2024-10-12 01:58:07 +0900

    add: 수정 및 추가 기능 구현

--- src/main/java/me/chanwoong/springbootdeveloper/controller/BlogViewController.java
+++ src/main/java/me/chanwoong/springbootdeveloper/controller/BlogViewController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -35,5 +36,20 @@ public class BlogViewController {
         model.addAttribute("article", new ArticleViewResponse(article));
 
         return "article";
+    }
+
+    @GetMapping("/new-article")
+    public String newArticle(@RequestParam(required = false) Long id, Model model) {
+        // 게시글 생성과 수정을 id 기준으로 없으면 생성 화면, 있으면 수정 화면으로 쓸려고 한다.
+        // 각 페이지를 안 만들고 재활용 느낌
+
+        if (id == null) {
+            model.addAttribute("article", new ArticleViewResponse());
+        } else {
+            Article article = blogService.findById(id);
+            model.addAttribute("article", new ArticleViewResponse(article));
+        }
+
+        return "newArticle";
     }
 }
